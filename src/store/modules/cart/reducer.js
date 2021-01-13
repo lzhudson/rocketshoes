461f7dcb8743129b6d37d73b0ec80c73089edd96
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-13 13:13:58 -0300

    feat: add action to remove product from cart

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -4,7 +4,7 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
       return produce(state, (draft) => {
-        const productIndex = draft.findIndex((p) => p.id === action.product.id);
+        const productIndex = draft.findIndex((p) => p.id === action.id);
         if (productIndex >= 0) {
           draft[productIndex].amount += 1;
         } else {
@@ -12,6 +12,13 @@ export default function cart(state = [], action) {
             ...action.product,
             amount: 1,
           });
+        }
+      });
+    case 'REMOVE_FROM_CART':
+      return produce(state, (draft) => {
+        const productIndex = draft.findIndex((p) => p.id === action.id);
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
         }
       });
     default:
