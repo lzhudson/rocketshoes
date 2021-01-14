7088e9993244035e674d64a82cb2555c5cdc6370
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-14 18:29:10 -0300

    refactor: add mapDispatchToProps to add actions component propps

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -4,10 +4,12 @@ import {
   MdAddCircleOutline,
   MdDelete,
 } from 'react-icons/md';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Container, ProductTable, Total } from './styles';
+import * as CartActions from '../../store/modules/cart/actions';
 
-function Cart({ cart, dispatch }) {
+function Cart({ cart, removeFromCart }) {
   return (
     <Container>
       <ProductTable>
@@ -22,7 +24,7 @@ function Cart({ cart, dispatch }) {
         </thead>
         <tbody>
           {cart.map((product) => (
-            <tr>
+            <tr key={product.id}>
               <td>
                 <img src={product.image} alt={product.title} />
               </td>
@@ -47,12 +49,7 @@ function Cart({ cart, dispatch }) {
               <td>
                 <button
                   type="button"
-                  onClick={() =>
-                    dispatch({
-                      type: 'REMOVE_FROM_CART',
-                      id: product.id,
-                    })
-                  }
+                  onClick={() => removeFromCart(product.id)}
                 >
                   <MdDelete size={20} color="#7159C1" />
                 </button>
@@ -71,7 +68,10 @@ function Cart({ cart, dispatch }) {
     </Container>
   );
 }
+const mapDispatchToProps = (dispatch) => {
+  bindActionCreators(CartActions, dispatch);
+};
 const mapStateToProps = (state) => ({
   cart: state.cart,
 });
-export default connect(mapStateToProps)(Cart);
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
